da5898e44ae78864aa6ffc52bd2adfa09ececce5
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2026-01-07 18:19:03 +0100

    Code improvement

--- process-worker-server/src/test/java/org/gridsuite/process/worker/server/core/ProcessTest.java
+++ process-worker-server/src/test/java/org/gridsuite/process/worker/server/core/ProcessTest.java
@@ -1,20 +1,18 @@
 package org.gridsuite.process.worker.server.core;
 
-import org.gridsuite.process.commons.SecurityAnalysisConfig;
-import org.gridsuite.process.worker.server.processes.securityanalysis.DummySecurityAnalysisService;
-import org.gridsuite.process.worker.server.processes.securityanalysis.SecurityAnalysisProcess;
+import org.gridsuite.process.commons.ProcessConfig;
+import org.gridsuite.process.commons.ProcessType;
 import org.gridsuite.process.worker.server.services.NetworkConversionService;
 import org.gridsuite.process.worker.server.services.NotificationService;
 import org.gridsuite.process.worker.server.services.StepExecutionService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -24,7 +22,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class ProcessTest {
     @Mock
-    private StepExecutionService<SecurityAnalysisConfig> stepExecutionService;
+    private StepExecutionService<ProcessConfig> stepExecutionService;
 
     @Mock
     private NotificationService notificationService;
@@ -33,24 +31,16 @@ class ProcessTest {
     private NetworkConversionService networkConversionService;
 
     @Mock
-    private DummySecurityAnalysisService securityAnalysisService;
+    private ProcessExecutionContext<ProcessConfig> processContext;
 
-    @Mock
-    private ProcessExecutionContext<SecurityAnalysisConfig> processContext;
-
-    @Captor
-    private ArgumentCaptor<UUID> previousStepIdCaptor;
-
-    private SecurityAnalysisProcess process;
+    private TestProcess process;
 
     @BeforeEach
     void setUp() {
-        process = new SecurityAnalysisProcess(
+        process = new TestProcess(
                 stepExecutionService,
                 notificationService,
-                networkConversionService,
-                securityAnalysisService
-        );
+                networkConversionService);
     }
 
     @Test
@@ -59,9 +49,9 @@ class ProcessTest {
         UUID step2Id = UUID.randomUUID();
         UUID step3Id = UUID.randomUUID();
 
-        ProcessStepExecutionContext<SecurityAnalysisConfig> stepContext1 = mock(ProcessStepExecutionContext.class);
-        ProcessStepExecutionContext<SecurityAnalysisConfig> stepContext2 = mock(ProcessStepExecutionContext.class);
-        ProcessStepExecutionContext<SecurityAnalysisConfig> stepContext3 = mock(ProcessStepExecutionContext.class);
+        ProcessStepExecutionContext<ProcessConfig> stepContext1 = mock(ProcessStepExecutionContext.class);
+        ProcessStepExecutionContext<ProcessConfig> stepContext2 = mock(ProcessStepExecutionContext.class);
+        ProcessStepExecutionContext<ProcessConfig> stepContext3 = mock(ProcessStepExecutionContext.class);
 
         when(stepContext1.getStepExecutionId()).thenReturn(step1Id);
         when(stepContext2.getStepExecutionId()).thenReturn(step2Id);
@@ -76,7 +66,6 @@ class ProcessTest {
 
         verify(stepExecutionService, times(3)).executeStep(any(), any());
         verify(stepExecutionService, never()).skipStep(any(), any());
-        verify(processContext, times(3)).createStepContext(any(), previousStepIdCaptor.capture());
         // Verify previousStepId is correctly set
         InOrder inOrder = inOrder(processContext);
         inOrder.verify(processContext).createStepContext(any(), eq(null));
@@ -89,9 +78,9 @@ class ProcessTest {
         UUID step1Id = UUID.randomUUID();
         UUID step2Id = UUID.randomUUID();
         UUID step3Id = UUID.randomUUID();
-        ProcessStepExecutionContext<SecurityAnalysisConfig> stepContext1 = mock(ProcessStepExecutionContext.class);
-        ProcessStepExecutionContext<SecurityAnalysisConfig> stepContext2 = mock(ProcessStepExecutionContext.class);
-        ProcessStepExecutionContext<SecurityAnalysisConfig> stepContext3 = mock(ProcessStepExecutionContext.class);
+        ProcessStepExecutionContext<ProcessConfig> stepContext1 = mock(ProcessStepExecutionContext.class);
+        ProcessStepExecutionContext<ProcessConfig> stepContext2 = mock(ProcessStepExecutionContext.class);
+        ProcessStepExecutionContext<ProcessConfig> stepContext3 = mock(ProcessStepExecutionContext.class);
         when(stepContext1.getStepExecutionId()).thenReturn(step1Id);
         when(stepContext2.getStepExecutionId()).thenReturn(step2Id);
         when(stepContext3.getStepExecutionId()).thenReturn(step3Id);
@@ -110,11 +99,30 @@ class ProcessTest {
         verify(stepExecutionService).skipStep(eq(stepContext3), any());
         verify(stepExecutionService, times(1)).executeStep(any(), any());
         verify(stepExecutionService, times(2)).skipStep(any(), any());
-        verify(processContext, times(3)).createStepContext(any(), previousStepIdCaptor.capture());
         // Verify previousStepId is correctly set
         InOrder inOrder = inOrder(processContext);
         inOrder.verify(processContext).createStepContext(any(), eq(null));
         inOrder.verify(processContext).createStepContext(any(), eq(step1Id));
         inOrder.verify(processContext).createStepContext(any(), eq(step2Id));
+    }
+
+    /**
+     * Mock implementation of AbstractProcess for testing purposes
+     */
+    private static class TestProcess extends AbstractProcess<ProcessConfig> {
+
+        public TestProcess(StepExecutionService<ProcessConfig> stepExecutionService, NotificationService notificationService, NetworkConversionService networkConversionService) {
+            super(ProcessType.SECURITY_ANALYSIS, stepExecutionService, notificationService, networkConversionService);
+        }
+
+        @Override
+        protected List<ProcessStep<ProcessConfig>> defineSteps() {
+            // Return 3 dummy steps for testing
+            ProcessStep<ProcessConfig> step1 = mock(ProcessStep.class);
+            ProcessStep<ProcessConfig> step2 = mock(ProcessStep.class);
+            ProcessStep<ProcessConfig> step3 = mock(ProcessStep.class);
+
+            return List.of(step1, step2, step3);
+        }
     }
 }

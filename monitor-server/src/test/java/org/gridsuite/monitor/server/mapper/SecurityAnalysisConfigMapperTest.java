53d3643719efb6e47f7af0c00fd98615ba8bbe57
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2026-03-12 11:33:09 +0100

    Remove outdated test

--- monitor-server/src/test/java/org/gridsuite/monitor/server/mapper/SecurityAnalysisConfigMapperTest.java
+++ monitor-server/src/test/java/org/gridsuite/monitor/server/mapper/SecurityAnalysisConfigMapperTest.java
@@ -6,7 +6,6 @@
  */
 package org.gridsuite.monitor.server.mapper;
 
-import org.gridsuite.monitor.commons.PersistedProcessConfig;
 import org.gridsuite.monitor.commons.SecurityAnalysisConfig;
 import org.gridsuite.monitor.server.entities.SecurityAnalysisConfigEntity;
 import org.junit.jupiter.api.Test;
@@ -39,28 +38,5 @@ class SecurityAnalysisConfigMapperTest {
         assertThat(entity.getLoadflowParametersUuid()).isEqualTo(dto.loadflowParametersUuid());
         assertThat(entity.getModificationUuids()).isEqualTo(dto.modificationUuids());
         assertThat(entity.getProcessType()).isEqualTo(dto.processType());
-    }
-
-    @Test
-    void entityToPersistedConfig() {
-        SecurityAnalysisConfigEntity entity = new SecurityAnalysisConfigEntity(
-                UUID.randomUUID(),
-                UUID.randomUUID()
-        );
-        entity.setId(UUID.randomUUID());
-        entity.setModificationUuids(
-                List.of(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID())
-        );
-
-        PersistedProcessConfig persistedProcessConfig = mapper.toPersistedProcessConfigDto(entity);
-
-        assertThat(persistedProcessConfig).isNotNull();
-        assertThat(persistedProcessConfig.id()).isEqualTo(entity.getId());
-
-        SecurityAnalysisConfig sac = (SecurityAnalysisConfig) persistedProcessConfig.processConfig();
-
-        assertThat(sac.securityAnalysisParametersUuid()).isEqualTo(entity.getSecurityAnalysisParametersUuid());
-        assertThat(sac.loadflowParametersUuid()).isEqualTo(entity.getLoadflowParametersUuid());
-        assertThat(sac.modificationUuids()).isEqualTo(entity.getModificationUuids());
     }
 }

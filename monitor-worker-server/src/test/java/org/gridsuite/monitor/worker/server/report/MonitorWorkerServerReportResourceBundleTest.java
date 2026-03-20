d295a52d7a42bf5adf2af11bae907258a9b27d7c
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2026-03-20 10:42:52 +0100

    Add regression test

---
+++ monitor-worker-server/src/test/java/org/gridsuite/monitor/worker/server/report/MonitorWorkerServerReportResourceBundleTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.monitor.worker.server.report;
+
+import com.powsybl.commons.report.ReportResourceBundle;
+import org.junit.jupiter.api.Test;
+
+import java.util.ResourceBundle;
+import java.util.ServiceLoader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+/**
+ * @author Antoine Bouhours <antoine.bouhours at rte-france.com>
+ */
+class MonitorWorkerServerReportResourceBundleTest {
+
+    @Test
+    void reportResourceBundleIsRegisteredViaAutoService() {
+        ServiceLoader<ReportResourceBundle> loader = ServiceLoader.load(ReportResourceBundle.class);
+
+        assertThat(loader).anyMatch(bundle -> bundle instanceof MonitorWorkerServerReportResourceBundle);
+    }
+
+    @Test
+    void reportBundleIsFound() {
+        assertThatNoException().isThrownBy(() ->
+            ResourceBundle.getBundle(MonitorWorkerServerReportResourceBundle.BASE_NAME)
+        );
+    }
+}

2f639bf1714b79e540ea4677ef3c0ac604802674
Thang PHAM <phamthang37@gmail.com>
2026-04-10 21:08:27 +0200

    Parameterized CRUD Test for ProcessConfig
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

---
+++ monitor-server/src/test/java/org/gridsuite/monitor/server/dataprovider/processconfig/ProcessConfigProviders.java
@@ -0,0 +1,21 @@
+package org.gridsuite.monitor.server.dataprovider.processconfig;
+
+import java.util.List;
+
+public final class ProcessConfigProviders {
+
+    private ProcessConfigProviders() {
+    }
+
+    private static List<ProcessConfigProvider<?, ?>> providers;
+
+    public static List<ProcessConfigProvider<?, ?>> all() {
+        if (providers == null) {
+            providers = List.of(
+                    new LoadFlowConfigProvider(),
+                    new SecurityAnalysisConfigProvider());
+        }
+
+        return providers;
+    }
+}
---
+++ monitor-server/src/test/java/org/gridsuite/monitor/server/dataprovider/processconfig/ProcessConfigTestDataFactory.java
@@ -0,0 +1,42 @@
+package org.gridsuite.monitor.server.dataprovider.processconfig;
+
+import org.gridsuite.monitor.commons.types.processconfig.LoadFlowConfig;
+import org.gridsuite.monitor.commons.types.processconfig.SecurityAnalysisConfig;
+import org.gridsuite.monitor.server.entities.processconfig.LoadFlowConfigEntity;
+import org.gridsuite.monitor.server.entities.processconfig.SecurityAnalysisConfigEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+public final class ProcessConfigTestDataFactory {
+
+    private ProcessConfigTestDataFactory() {
+    }
+
+    public static List<UUID> modifications() {
+        return List.of(UUID.randomUUID(), UUID.randomUUID());
+    }
+
+    public static LoadFlowConfigEntity loadFlowEntity() {
+        LoadFlowConfigEntity e = new LoadFlowConfigEntity();
+        e.setModificationUuids(modifications());
+        e.setLoadflowParametersUuid(UUID.randomUUID());
+        return e;
+    }
+
+    public static LoadFlowConfig loadFlowDto() {
+        return new LoadFlowConfig(UUID.randomUUID(), modifications());
+    }
+
+    public static SecurityAnalysisConfigEntity securityAnalysisEntity() {
+        SecurityAnalysisConfigEntity e = new SecurityAnalysisConfigEntity();
+        e.setModificationUuids(modifications());
+        e.setSecurityAnalysisParametersUuid(UUID.randomUUID());
+        e.setLoadflowParametersUuid(UUID.randomUUID());
+        return e;
+    }
+
+    public static SecurityAnalysisConfig securityAnalysisDto() {
+        return new SecurityAnalysisConfig(UUID.randomUUID(), modifications(), UUID.randomUUID());
+    }
+}

5ffedd04fd78d4bf05c000b06e28f6f2898302b8
Thang PHAM <phamthang37@gmail.com>
2026-04-12 23:20:44 +0200

    clean unused method
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- monitor-server/src/test/java/org/gridsuite/monitor/server/testdata/processconfig/ProcessConfigTestDataProvider.java
+++ monitor-server/src/test/java/org/gridsuite/monitor/server/testdata/processconfig/ProcessConfigTestDataProvider.java
@@ -3,28 +3,17 @@ package org.gridsuite.monitor.server.testdata.processconfig;
 import org.gridsuite.monitor.commons.types.processconfig.ProcessConfig;
 import org.gridsuite.monitor.server.entities.processconfig.ProcessConfigEntity;
 
-import java.util.UUID;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public interface ProcessConfigTestDataProvider<D extends ProcessConfig, E extends ProcessConfigEntity> {
-    // --- dto side---
+public interface ProcessConfigTestDataProvider<D extends ProcessConfig, E extends ProcessConfigEntity> {-
+
     D createDto();
 
-    D updateDto(D dto);
-
-    // --- entity side ---
     E createEntity();
-
-    E updateEntity(E entity);
 
     Class<E> entityType();
 
     Class<D> dtoType();
-
-    default UUID getId(E entity) {
-        return entity.getId();
-    }
 
     default void assertEntityEquals(E expected, E actual) {
         assertThat(actual)

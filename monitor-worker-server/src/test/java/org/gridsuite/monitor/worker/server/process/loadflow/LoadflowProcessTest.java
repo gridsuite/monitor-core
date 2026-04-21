4c70f7bae04268d0e2b64a66aad9d4ab7aaaf007
Caroline Jeandat <caroline.jeandat@rte-france.com>
2026-04-21 21:22:52 +0200

    implement abstract test class for processes

--- monitor-worker-server/src/test/java/org/gridsuite/monitor/worker/server/process/loadflow/LoadflowProcessTest.java
+++ monitor-worker-server/src/test/java/org/gridsuite/monitor/worker/server/process/loadflow/LoadflowProcessTest.java
@@ -8,26 +8,23 @@ package org.gridsuite.monitor.worker.server.process.loadflow;
 
 import org.gridsuite.monitor.commons.types.processconfig.LoadFlowConfig;
 import org.gridsuite.monitor.commons.types.processexecution.ProcessType;
+import org.gridsuite.monitor.worker.server.core.process.AbstractProcessTest;
 import org.gridsuite.monitor.worker.server.core.process.ProcessStep;
 import org.gridsuite.monitor.worker.server.process.commons.steps.ApplyModificationsStep;
 import org.gridsuite.monitor.worker.server.process.commons.steps.LoadNetworkStep;
 import org.gridsuite.monitor.worker.server.process.loadflow.steps.LoadflowRunComputationStep;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertSame;
-
 /**
  * @author Caroline Jeandat {@literal <caroline.jeandat at rte-france.com>}
  */
 @ExtendWith(MockitoExtension.class)
- class LoadflowProcessTest {
+class LoadflowProcessTest extends AbstractProcessTest<LoadFlowConfig, LoadflowProcess> {
 
     @Mock
     private LoadNetworkStep<LoadFlowConfig> loadNetworkStep;
@@ -38,10 +35,9 @@ import static org.junit.jupiter.api.Assertions.assertSame;
     @Mock
     private LoadflowRunComputationStep runComputationStep;
 
-    private LoadflowProcess process;
-
+    @Override
     @BeforeEach
-    void setUp() {
+    protected void setUp() {
         process = new LoadflowProcess(
             loadNetworkStep,
             applyModificationsStep,
@@ -49,19 +45,17 @@ import static org.junit.jupiter.api.Assertions.assertSame;
         );
     }
 
-    @Test
-    void getProcessTypeShouldReturnLoadflow() {
-        assertEquals(ProcessType.LOADFLOW, process.getProcessType());
+    @Override
+    protected List<ProcessStep<LoadFlowConfig>> getExpectedSteps() {
+        return List.of(
+            loadNetworkStep,
+            applyModificationsStep,
+            runComputationStep
+        );
     }
 
-    @Test
-    void defineStepsShouldReturnThreeStepsInCorrectOrder() {
-        List<ProcessStep<LoadFlowConfig>> steps = process.defineSteps();
-
-        assertNotNull(steps);
-        assertEquals(3, steps.size());
-        assertSame(loadNetworkStep, steps.get(0));
-        assertSame(applyModificationsStep, steps.get(1));
-        assertSame(runComputationStep, steps.get(2));
+    @Override
+    protected ProcessType getExpectedProcessType() {
+        return ProcessType.LOADFLOW;
     }
 }

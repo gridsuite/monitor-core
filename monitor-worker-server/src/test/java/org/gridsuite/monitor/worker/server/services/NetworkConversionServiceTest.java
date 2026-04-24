67ec2692fea44f582fc64a62ef40362c1d3fda81
Antoine Bouhours <antoine.bouhours@rte-france.com>
2026-04-24 08:54:51 +0200

    Use Network.read() instead of manual implementation (#85)

--- monitor-worker-server/src/test/java/org/gridsuite/monitor/worker/server/services/NetworkConversionServiceTest.java
+++ monitor-worker-server/src/test/java/org/gridsuite/monitor/worker/server/services/NetworkConversionServiceTest.java
@@ -6,13 +6,9 @@
  */
 package org.gridsuite.monitor.worker.server.services;
 
-import com.powsybl.cases.datasource.CaseDataSourceClient;
-import com.powsybl.commons.PowsyblException;
+import com.powsybl.commons.datasource.ReadOnlyDataSource;
 import com.powsybl.commons.report.ReportNode;
-import com.powsybl.computation.ComputationManager;
-import com.powsybl.iidm.network.Importer;
 import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.NetworkFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -24,7 +20,6 @@ import java.util.Properties;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -35,9 +30,6 @@ import static org.mockito.Mockito.*;
 class NetworkConversionServiceTest {
     @Autowired
     private NetworkConversionService service;
-
-    @Mock
-    private Importer importer;
 
     @Mock
     private Network network;
@@ -53,37 +45,15 @@ class NetworkConversionServiceTest {
     }
 
     @Test
-    void createNetworkShouldImportNetworkWhenImporterFound() {
-        try (MockedStatic<Importer> importerMock = mockStatic(Importer.class)) {
-            importerMock.when(() -> Importer.find(any(CaseDataSourceClient.class), any()))
-                    .thenReturn(importer);
-            when(importer.importData(
-                    any(CaseDataSourceClient.class),
-                    any(NetworkFactory.class),
-                    any(Properties.class),
-                    eq(reportNode)
-            )).thenReturn(network);
+    void createNetworkShouldReadNetwork() {
+        try (MockedStatic<Network> networkMock = mockStatic(Network.class)) {
+            networkMock.when(() -> Network.read(any(ReadOnlyDataSource.class), any(Properties.class), any(ReportNode.class)))
+                    .thenReturn(network);
 
             Network result = service.createNetwork(caseUuid, reportNode);
 
             assertThat(result).isSameAs(network);
-            verify(importer).importData(
-                    any(CaseDataSourceClient.class),
-                    any(NetworkFactory.class),
-                    any(Properties.class),
-                    eq(reportNode)
-            );
-        }
-    }
-
-    @Test
-    void createNetworkShouldThrowExceptionWhenNoImporterFound() {
-        try (MockedStatic<Importer> importerMock = mockStatic(Importer.class)) {
-            importerMock.when(() -> Importer.find(any(CaseDataSourceClient.class), any(ComputationManager.class))).thenReturn(null);
-
-            assertThatThrownBy(() -> service.createNetwork(caseUuid, reportNode))
-                    .isInstanceOf(PowsyblException.class)
-                    .hasMessage("No importer found");
+            networkMock.verify(() -> Network.read(any(ReadOnlyDataSource.class), any(Properties.class), any(ReportNode.class)));
         }
     }
 }
